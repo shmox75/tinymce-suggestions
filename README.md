000aca80b78d8190a5cf51109eb000da5d906036
shmox75 <shmox75@gmail.com>
2023-05-26 11:29:09 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
-# tinymce-suggestions
-Autocomplete plugin for TinyMCE
+# TinyMCE suggestions
+TinyMCE suggestions is a an auto-complete plugin.
+
+This plugin doesn't need a special char to trigger the suggestion function and doesn't popup a list of suggestions.
+Instead this plugin is only triggered by typing.
+
+To retrieve the suggested word completion, this plugin searches from a list of words, or through a user defined function, for example to search from an API.
+
+The completion function can be triggered by pressing the `Tab` key in the keyboard.
+
+![TinyMCE suggestions screenshot](image.png)
+
+### Demo
+
+INSERT LINK HERE
+
+### Features
+
+* LOL
+* Snoopy
+
+### Example
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions"
+});
+```
+
+### Options
+These options affect the execution of the suggestions of the `suggestions` plugin.
+
+### `terms_list`
+This option is an array containing a list of `string` terms where the plugin will search to complete the typed word.
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    terms_list: [
+        'Afghanistan',
+        'Albania',
+        'Algeria',
+        'Andorra'
+    ]
+});
+```
+
+### `min_search_length`
+This option is an integer which represent the minimum word length before triggering the suggestion function.
+The default value is `3`.
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    min_search_length: 3
+});
+```
+
+### `suggestion_node_id`
+This option is the id of the TinyMCE DOM node containing the suggestion.
+The default value is `suggestionNodeId`
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    suggestion_node_id: "suggestionNodeId"
+});
+```
+
+### `suggestion_text_color`
+This option defines the html hex color of the suggestion completion text.
+The default value is `#dee7f1`.
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    suggestion_text_color: "#dee7f1"
+});
+```
+
+### `debounce_time`
+This option is an integer which defines the delay in milliseconds which the plugin waits when you type before triggering the suggestion function.
+The default value is `250` ms.
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    debounce_time: 250
+});
+```
+
+### `search_function`
+This option is a user defined `async` function which is used to get the suggested word.
+The return value is a string or an array of strings.
+The `keyword` argument of the function represents the word already typed in TinyMCE editor.
+The default value is `null`.
+
+```javascript
+tinymce.init({
+    selector: "#textarea",  // change this value according to your HTML
+    plugins: "suggestions",
+    search_function: async ( keyword ) => {
+        const response = await fetch(`https://example.com/api/?keyword=${keyword}`);
+        
+        if(response.ok){
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }else{
+           throw new Error("ERR")
+        }
+    }
+});
+```
---
+++ plugin.js
@@ -0,0 +1,204 @@
+
+tinymce.PluginManager.add('suggestions', function(editor) {
+    var VK = tinymce.util.VK;
+
+    const searchFunction = editor.getParam("search_function", null);
+    const debounceTime = editor.getParam("debounce_time", 250); //ms
+    const suggestionTextColor = editor.getParam("suggestion_text_color", '#dee7f1'); //hex
+    const suggestionNodeId = editor.getParam("suggestion_node_id", 'suggestionNodeId');
+    const minLengthSearch = editor.getParam("min_search_length", 3);
+    const terms_list = editor.getParam("terms_list", []);
+
+    let suggestionNode;
+	let waiting = false;
+	let waitingText;
+
+    const excludedKeys = [
+        'Space',
+        'Enter',
+        'NumpadEnter',
+        'Delete',
+        'Escape'      
+    ]
+
+    const neutralKeys = [
+        'ShiftLeft',
+        'ShiftRight',
+        'AltRight',
+        'AltLeft',
+        'ControlLeft',
+        'ControlRight'
+    ]
+
+    function debounce(cb, delay = debounceTime) {
+        let timeout
+
+        return (...args) => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+            cb(...args)
+            }, delay)
+        }
+    }
+
+    async function findWord( txt ) {
+        if(searchFunction == null){
+            return terms_list.find(elm => elm.toLocaleLowerCase().startsWith(txt.toLocaleLowerCase()));
+        }else{
+            let result = await searchFunction(txt);
+
+            if(Array.isArray(result)){
+                return result.find(elm => elm.toLocaleLowerCase().startsWith(txt.toLocaleLowerCase()));
+            }else{
+                return result.toLocaleLowerCase().startsWith(txt.toLocaleLowerCase()) ? result : undefined
+            }
+        }     
+    }
+
+    function cancelEvent(e){
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    function removeSuggestionNode(){
+        suggestionNode && suggestionNode.remove();
+        suggestionNode = undefined;
+    }
+
+    function getTheWordToCheck(theText, theSub){
+        let re = new RegExp(`${theSub}$`, "gi")
+        let minus = theText.replace(re, "");
+
+        return minus.split(' ').pop();			
+    }
+
+    function keyCheck( keyCode ){
+        return excludedKeys.indexOf(keyCode) == -1
+    }
+
+    function getCompletion(typed, word) {
+        let re = new RegExp(`^${typed}`, "gi")
+        
+        return word.replace(re, "");
+    }
+
+    function insertCompletionNode(ed, txt){
+        removeSuggestionNode();
+
+        let rng = ed.selection.getRng(1);
+        
+        suggestionNode = ed.getDoc().createElement("span");
+        suggestionNode.style.color = suggestionTextColor;
+        suggestionNode.innerHTML = txt;
+        suggestionNode.id = suggestionNodeId;
+        let offset =  ed.selection.getSel().focusOffset;
+        rng.insertNode(suggestionNode);
+
+        let nodeSuggestion = ed.dom.select(`#${suggestionNodeId}`);
+
+        if(nodeSuggestion[0].lastChild != null){
+            ed.selection.setCursorLocation(nodeSuggestion[0].firstChild, 0);					
+        }
+
+        ed.selection.collapse(0);
+
+        waiting = true;
+        waitingText = txt;
+    }
+
+    function resetSuggestion() {
+        waiting = false;
+        waitingText = undefined;
+    }
+
+    function completeWord(ed) {
+        removeSuggestionNode();
+        ed.execCommand('mceInsertContent', false, waitingText);
+        ed.selection.collapse(1);
+        
+        resetSuggestion();
+    }
+
+    function changeSuggestionDom(ed, key){
+        if(waitingText && waitingText.toLocaleLowerCase().startsWith(key.toLocaleLowerCase())){
+            waitingText = waitingText.substring(1);
+            removeSuggestionNode()
+            insertCompletionNode(ed, waitingText)
+        }else{
+            removeSuggestionNode()
+            resetSuggestion();
+        }
+    }
+
+    async function startProcess(ed){
+        let offset =  ed.selection.getSel().focusOffset;
+        let rng = ed.selection.getRng(1);
+        let theText = rng.startContainer.data;
+
+        if(theText){
+            let theSub = theText.substring(offset);
+
+            if(theSub.length == 0 || theSub.match(/^\s/g)){
+                const wordToCheck = getTheWordToCheck(theText, theSub);
+                if(wordToCheck.length >= minLengthSearch){
+                    let wordFound = await findWord(wordToCheck);
+                    if(wordFound != undefined){
+                        let substr = getCompletion(wordToCheck, wordFound)
+                        insertCompletionNode(ed, substr);
+                    }							
+                }
+            }else{
+                //console.log(theSub.charAt(0));
+            }
+        }else{
+            //TODO
+        }				
+    }
+
+    let process = debounce(async function(event, editor) {
+        if(keyCheck(event.code)){
+            startProcess(editor)
+        }
+
+        var name = event.key;
+        var char = event.code;
+    });
+
+    editor.on('keyup', (event) => {
+        if(!waiting){
+            process(event, editor)
+        }else{
+            /*if(keyCheck(event.code)){
+                //console.log('TODO', event.key);
+                
+            }else{
+                removeSuggestionNode();
+                resetSuggestion();
+            }*/							
+        }						
+    });
+
+    editor.on('keydown', (event) => { //console.log(event.code);
+        if(event.code == 'Tab' && waiting){
+            cancelEvent(event)
+            completeWord(editor)
+        }else{
+            if(!keyCheck(event.code)){
+                removeSuggestionNode();
+                resetSuggestion();
+            }else if(waiting && neutralKeys.indexOf(event.code) == -1){
+                changeSuggestionDom(editor, event.key)
+            }
+        }
+    });
+
+    editor.on('blur', (e) => {
+        removeSuggestionNode();
+        resetSuggestion();
+    })	
+    
+    editor.on('click', (e) => {
+        removeSuggestionNode();
+        resetSuggestion();
+    })
+});
